62c7ba87a54fbab4c980b2a41b36524ff0d364c0
Robert Zavala <robert@algaecal.com>
2022-02-22 18:47:31 -0700

    edit server

--- http-server/index.js
+++ http-server/index.js
@@ -22,16 +22,26 @@ const friends = [
 server.on('request', (req, res) => {
   const items = req.url.split('/');
 
-  if (items[1] === 'friends') {
+  if (req.method === 'POST' && items[1] === 'friends') {
+    res.setHeader('Content-Security-Policy', 'default-src')
+
+    req.on('data', (data) => {
+      const friend = data.toString();
+      console.log('Request:', friend);
+      friends.push(JSON.parse(friend));
+    })
+    req.pipe(res);
+  } else if (req.method === 'GET' && items[1] === 'friends') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
+
     if (items.length === 3) {
       const friendIndex = Number(items[2]);
       res.end(JSON.stringify(friends[friendIndex]));
     } else {
       res.end(JSON.stringify(friends));
     }
-  } else if (items[1] === 'messages') {
+  } else if (req.method === 'GET' && items[1] === 'messages') {
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>');
     res.write('<body>');
