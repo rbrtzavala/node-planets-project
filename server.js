891668c402e1d55b9ee627ba35f7f2183bf85e84
Robert Zavala <robert@algaecal.com>
2022-02-22 19:17:35 -0700

    added express to project

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3030;
+
+const friends = [
+  {
+    id: 0,
+    name: 'Nikola Tesla',
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Newton',
+  },
+  {
+    id: 2,
+    name: 'Albert Einstein',
+  }
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends)
+})
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend)
+  }
+  else {
+    res.status(404).json({
+      error: 'Friend Does not exist'
+    });
+  }
+})
+
+app.get('/messages', (req, res) => {
+  res.send('<ul><li>MESSAGES</li></ul>')
+})
+
+app.post('/MESSAGES', (req, res) => {
+  console.log('Updating messages...')
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+})
