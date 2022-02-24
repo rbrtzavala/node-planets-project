ebb0a9c4f1df44f76045320f73ed078d247cf705
Robert Zavala <robert@algaecal.com>
2022-02-23 18:07:51 -0700

    refactor routes to own directory

--- server.js
+++ server.js
@@ -1,42 +1,24 @@
 const express = require('express');
 
-const friendsController = require('./controllers/frineds.controller');
-const messagesController = require('./controllers/messages.controller');
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router')
 
 const app = express();
 
 const PORT = 3000;
 
-const friends = [
-  {
-    id: 0,
-    name: 'Nikola Tesla',
-  },
-  {
-    id: 1,
-    name: 'Sir Isaac Newton',
-  },
-  {
-    id: 2,
-    name: 'Albert Einstein',
-  }
-];
-
 app.use((req, res, next) => {
   const start = Date.now();
   next();
   const delta = Date.now() - start;
-  console.log(`${delta}ms: ${req.method} ${req.url}`);
+  console.log(`${req.method}: ${req.baseUrl}${req.url} > ${delta}ms:`);
 });
 
 app.use(express.json());
 
-app.post('/friends', friendsController.postFriend);
-app.get('/friends', friendsController.getFriends);
-app.get('/friends/:friendId', friendsController.getFriend);
+app.use('/friends', friendsRouter);
 
-app.get('/messages', messagesController.getMessages);
-app.post('/messages', messagesController.postMessage);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
